ac986fe7b09bdfaccfd4397d7fc172482fde2fec
Andrew Monshizadeh <amonshiz@thebrowser.company>
2023-01-25 14:42:58 -0500

    Initial Commit

---
+++ RUsageGrabber/RUsageGrabberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RUsageGrabberApp.swift
+//  RUsageGrabber
+//
+//  Created by Andrew Monshizadeh on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RUsageGrabberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
